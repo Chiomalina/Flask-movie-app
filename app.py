5174543d69470314f106453c773cfe40154cd4b8
Lina Chioma Anaso <linachiomaanaso@Chioma-Personal-MacBook-Air.local>
2025-12-05 19:54:02 +0100

    create 404.html

--- app.py
+++ app.py
@@ -138,5 +138,15 @@ def delete_movie(user_id, movie_id):
 	return redirect(url_for("user_movies", user_id=user_id))
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_error(e):
+	return render_template("500.html"), 500
+
+
 if __name__ == "__main__":
 	app.run(debug=True)
